201cf2ddd9005372754e5cd82aedfea063eb7037
Cong Nguyen <cong.ndt.24@gmail.com>
2026-05-05 16:18:34 +0700

    Refactor filename construction using path.join

--- secondChance-backend/util/import-mongo/index.js
+++ secondChance-backend/util/import-mongo/index.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 const MongoClient = require('mongodb').MongoClient
 const fs = require('fs')
+const path = require('path')
 
 // MongoDB connection URL with authentication options
 const url = process.env.MONGO_URL
-const filename = `${__dirname}/secondChanceItems.json`
+const filename = path.join(__dirname, 'secondChanceItems.json')
 const dbName = 'secondChance'
 const collectionName = 'secondChanceItems'
 
@@ -12,7 +13,7 @@ const collectionName = 'secondChanceItems'
 const data = JSON.parse(fs.readFileSync(filename, 'utf8')).docs
 
 // connect to database and insert data into the collection
-async function loadData() {
+async function loadData () {
   const client = new MongoClient(url)
 
   try {
